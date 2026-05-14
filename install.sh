d50c0050496cdb4d6b6e2847ac1d7be9d8b9aab1
Haisam Ido <haisam.ido@gmail.com>
2026-05-14 14:48:15 -0400

    Refactored install.sh into work() and home() environment functions
    Structure:
    - Moved brew formulae and casks into work() function matching current system state
    - Created home() function that calls work() then adds home-only casks
    - Added environment selector prompt at runtime (1=work, 2=home)
    - Added confirmation guard when selecting home
    
    work() formulae added (not previously in script):
    - act, espeak-ng, ipython, jupytext
    
    work() casks added:
    - freecad, inkscape, lens, pgadmin4, postgres-app, tigervnc
    
    work() casks removed (moved to home-only):
    - ableton-live-suite, android-platform-tools, audacity, blender, docker,
      firefox, google-chrome, lmms, openscad, reaper, steam, surge-xt
    
    work() casks removed entirely:
    - db-browser-for-sqlite, google-driv, mysqlworkbench
    
    home() casks added (new):
    - arduino, google-drive, surge-xt
    
    Fixes:
    - Fixed amazon-ecs-cli (was mazon-ecs-cli)
    - Fixed google-driv typo to google-drive (in home)
    - Fixed trailing whitespace on brew upgrade line

--- install.sh
+++ install.sh
@@ -40,156 +40,192 @@ brew update && brew cleanup
 
 # Upgrade applications if there are any
 #brew outdated | xargs brew upgrade
-brew upgrade 
+brew upgrade
 brew upgrade --cask
 
-brew install mazon-ecs-cli \
-  ansible \
-  autoconf \
-  aws-shell \
-  awscli \
-  awslogs \
-  bash-completion@2 \
-  cask \
-  cmake \
-  colordiff \
-  container-diff \
-  couchdb \
-  cspice \
-  curl \
-  direnv \
-  dive \
-  docker \
-  docker-squash \
-  docutils \
-  dos2unix \
-  duckdb \
-  emscripten \
-  fluid-synth \
-  fluxcd/tap/flux \
-  gawk \
-  gh \
-  git \
-  gitlab-ci-local \
-  gnu-getopt \
-  gnu-sed \
-  gnuplot \
-  go-md2man \
-  go-task \
-  gradle \
-  graphviz \
-  grep \
-  hashicorp/tap/terraform \
-  hiredis \
-  htop \
-  iftop \
-  jansson \
-  javacc \
-  jq \
-  jupyter-r \
-  k3sup \
-  k9s \
-  kind \
-  kompose \
-  kreuzwerker/taps/m1-terraform-provider-helper \
-  kubectx \
-  kustomize \
-  lazydocker \
-  libfido2 \
-  lilypond \
-  links \
-  lynx \
-  make \
-  maven \
-  memcached \
-  midnight-commander \
-  minikube \
-  mysql \
-  nano \
-  netcat \
-  nmap \
-  node@20 \
-  nvm \
-  octave \
-  openconnect \
-  openjdk@21 \
-  openvpn \
-  p7zip \
-  parallel \
-  peco \
-  pidof \
-  pipx \
-  podman-compose \
-  poppler \
-  postgresql@15 \
-  pulumi/tap/pulumi \
-  pyqt \
-  pyqt@5 \
-  python-matplotlib \
-  qemu \
-  qt \
-  redis \
-  rpm2cpio \
-  rsync \
-  ruby \
-  rust \
-  s3cmd \
-  scrcpy \
-  socket_vmnet \
-  speedtest-cli \
-  swig \
-  tcpdump \
-  terragrunt \
-  tig \
-  tmux \
-  txn2/tap/kubefwd \
-  uv \
-  virt-manager \
-  watch \
-  wget \
-  wxwidgets \
-  wxwidgets@3.2 \
-  xcodes \
-  xerces-c \
-  xq \
-  yq \
-  zsync
+work() {
+  brew install act \
+    amazon-ecs-cli \
+    ansible \
+    autoconf \
+    aws-shell \
+    awscli \
+    awslogs \
+    bash-completion@2 \
+    cask \
+    cmake \
+    colordiff \
+    container-diff \
+    couchdb \
+    cspice \
+    curl \
+    direnv \
+    dive \
+    docker \
+    docker-squash \
+    docutils \
+    dos2unix \
+    duckdb \
+    emscripten \
+    espeak-ng \
+    fluid-synth \
+    fluxcd/tap/flux \
+    gawk \
+    gh \
+    git \
+    gitlab-ci-local \
+    gnu-getopt \
+    gnu-sed \
+    gnuplot \
+    go-md2man \
+    go-task \
+    gradle \
+    graphviz \
+    grep \
+    hashicorp/tap/terraform \
+    hiredis \
+    htop \
+    iftop \
+    ipython \
+    jansson \
+    javacc \
+    jq \
+    jupyter-r \
+    jupytext \
+    k3sup \
+    k9s \
+    kind \
+    kompose \
+    kreuzwerker/taps/m1-terraform-provider-helper \
+    kubectx \
+    kustomize \
+    lazydocker \
+    libfido2 \
+    lilypond \
+    links \
+    lynx \
+    make \
+    maven \
+    memcached \
+    midnight-commander \
+    minikube \
+    mysql \
+    nano \
+    netcat \
+    nmap \
+    node@20 \
+    nvm \
+    octave \
+    openconnect \
+    openjdk@21 \
+    openvpn \
+    p7zip \
+    parallel \
+    peco \
+    pidof \
+    pipx \
+    podman-compose \
+    poppler \
+    postgresql@15 \
+    pulumi/tap/pulumi \
+    pyqt \
+    pyqt@5 \
+    python-matplotlib \
+    qemu \
+    qt \
+    redis \
+    rpm2cpio \
+    rsync \
+    ruby \
+    rust \
+    s3cmd \
+    scrcpy \
+    socket_vmnet \
+    speedtest-cli \
+    swig \
+    tcpdump \
+    terragrunt \
+    tig \
+    tmux \
+    txn2/tap/kubefwd \
+    uv \
+    virt-manager \
+    watch \
+    wget \
+    wxwidgets \
+    wxwidgets@3.2 \
+    xcodes \
+    xerces-c \
+    xq \
+    yq \
+    zsync
 
-brew install --cask \
-  ableton-live-suite \
-  adobe-acrobat-reader \
-  android-platform-tools \
-  audacity \
-  blender \
-  db-browser-for-sqlite \
-  dbeaver-community \
-  docker \
-  eclipse-ide \
-  firefox \
-  github \
-  google-chrome \
-  google-driv \
-  headlamp \
-  lmms \
-  mysqlworkbench \
-  openscad \
-  podman-desktop \
-  pgadmin4 \
-  postgres-unofficial \
-  processing \
-  quarto \
-  reaper \
-  rstudio \
-  session-manager-plugin \
-  steam \
-  sublime-text \
-  tigervnc-viewer \
-  utm \
-  vagrant \
-  virtualbox \
-  visual-studio-code \
-  vlc \
-  xquartz
+  brew install --cask \
+    adobe-acrobat-reader \
+    dbeaver-community \
+    eclipse-ide \
+    freecad \
+    github \
+    headlamp \
+    inkscape \
+    lens \
+    pgadmin4 \
+    podman-desktop \
+    postgres-app \
+    postgres-unofficial \
+    processing \
+    quarto \
+    rstudio \
+    session-manager-plugin \
+    sublime-text \
+    tigervnc \
+    tigervnc-viewer \
+    utm \
+    vagrant \
+    virtualbox \
+    visual-studio-code \
+    vlc \
+    xquartz
+}
+
+home() {
+  work
+
+  brew install --cask \
+    ableton-live-suite \
+    android-platform-tools \
+    arduino \
+    audacity \
+    blender \
+    docker \
+    firefox \
+    google-chrome \
+    google-drive \
+    lmms \
+    openscad \
+    reaper \
+    steam \
+    surge-xt
+}
+
+echo "Select environment:"
+echo "  1) work"
+echo "  2) home"
+read -r -p "Choice [1/2]: " choice
+
+case "${choice}" in
+  1) work ;;
+  2)
+    read -r -p "Are you sure you want to install home packages? [y/N]: " confirm
+    if [[ "${confirm}" =~ ^[Yy]$ ]]; then
+      home
+    else
+      echo "Aborted."
+      exit 0
+    fi
+    ;;
+  *) echo "Invalid choice"; exit 1 ;;
+esac
 
 # Python stuff
 #pip3 install --user bitstring uritools nose tornado boto3 lz4tools genson pypi matplotlib numpy py-gnuplot debugpy
